c37989b38d6375e2b36cdc8352ea223b4804cdb0
Patryck Sans <89539370+PatryckSans@users.noreply.github.com>
2021-10-21 17:48:57 -0300

    config da ferramenta remove (#1)

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const DevControllers = require('./controllers/DevController');
+const DevController = require('./controllers/DevController');
 const LikeControllers = require('./controllers/LikeController');
 const DislikeControllers = require('./controllers/DislikeController');
-const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-
-routes.get('/devs', DevControllers.index);
-routes.post('/devs', DevControllers.store);
+routes.delete('/devs/:devId', DevController.remove);
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeControllers.store);
 routes.post('/devs/:devId/dislikes', DislikeControllers.store);
 
